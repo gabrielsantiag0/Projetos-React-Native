704656f845f1a62d6f2c3653ecfc8c1ce9be604f
gabrielsantiag0 <gabrielleandrosantiago9@gmail.com>
2024-04-24 15:07:01 -0300

    I finished the icons and avatar, where I was having difficulty, after a lot of research I'm moving forward with the project

--- RnCRud/src/data/Users.js
+++ RnCRud/src/data/Users.js
@@ -4,34 +4,34 @@ export default [
         name: 'Gabriel Santiago',
         email: 'gabrielleandrosantiago@gmil.com',
         avatarUrl:
-        'https://pixabay.com/pt/vectors/avatar-bico-preto-bonitinho-emo%C3%A7%C3%A3o-12954'
+        'https://cdn.pixabay.com/photo/2016/04/01/10/04/amusing-1299756_1280.png'
     },
     {
         id: 2, 
         name: 'Gustavo Santiago',
         email: 'gustavohenriquesantiago@gmil.com',
         avatarUrl:
-        'https://pixabay.com/pt/vectors/avatar-bico-preto-bonitinho-emo%C3%A7%C3%A3o-12954'
+        'https://cdn.pixabay.com/photo/2016/03/31/19/58/avatar-1295429_1280.png'
     },
     {
         id: 3, 
         name: 'Kleber Santiago',
         email: 'kleberbitosantiago@gmil.com',
         avatarUrl:
-        'https://pixabay.com/pt/vectors/avatar-bico-preto-bonitinho-emo%C3%A7%C3%A3o-12954'
+        'https://cdn.pixabay.com/photo/2013/07/12/19/15/gangster-154425_1280.png'
     },
     {
         id:4, 
         name: 'Kelly Cristina',
         email: 'kellyCristina@gmil.com',
         avatarUrl:
-       'https://pixabay.com/pt/vectors/avatar-bico-preto-bonitinho-emo%C3%A7%C3%A3o-12954'
+        'https://cdn.pixabay.com/photo/2023/04/02/00/55/woman-7893318_1280.png'
     },
     {
         id:5, 
         name: 'Yasmin Nathalin',
         email: 'yasminNathalin@gmil.com',
         avatarUrl:
-       'https://pixabay.com/pt/vectors/avatar-bico-preto-bonitinho-emo%C3%A7%C3%A3o-12954'
+      'https://cdn.pixabay.com/photo/2016/09/01/08/24/smiley-1635449_1280.png'
     },
 ]
--- RnCRud/src/views/UserList.js
+++ RnCRud/src/views/UserList.js
@@ -1,17 +1,39 @@
 import React from 'react'
 import { FlatList, Text, View } from 'react-native'
 import Users from '../data/Users'
-import { ListItem, Avatar } from '@rneui/themed';
-
+import { ListItem, Avatar, Icon } from '@rneui/themed';
+import { Button } from '@rneui/themed';
 
 
 export default props =>{
 
+    function confirmUserDeletion(User) {
+
+    }
+
     function getUserItem(  { item: Users }  ) {
         return (
-            <ListItem>
+            <ListItem 
+            bottomDivider
+            onPress={ () => props.navigation.navigate('UserForm') }
+          >
                 <Avatar
-                source={{uri: }}
+                source={{uri: Users.avatarUrl}}
+                key={Users.id}
+                />
+              <ListItem.Content>
+                <ListItem.Title>{Users.name}</ListItem.Title>
+                <ListItem.Subtitle>{Users.email}</ListItem.Subtitle>
+              </ListItem.Content>
+              <Button 
+                onPress={() => props.navigation.navigate('UserForm', Users)}
+                type="clear"
+                icon={<Icon name='edit' size={25} color='orange'/>}
+                />
+                <Button 
+                onPress={() => props.navigation.navigate('UserForm', Users)}
+                type="clear"
+                icon={<Icon name='delete' size={25} color='red'/>}
                 />
             </ListItem>
         )
