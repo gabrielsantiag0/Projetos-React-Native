b04560d383b72fd216ab076906cf621f00493e0a
gabrielsantiag0 <gabrielleandrosantiago9@gmail.com>
2024-05-23 20:09:22 -0300

    Create Folders

---
+++ AppVi/app/(auth)/sign-in.jsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const SingIn = () => {
+  return (
+    <View>
+      <Text>SingIn</Text>
+    </View>
+  )
+}
+
+export default SingIn
+
+const styles = StyleSheet.create({})
