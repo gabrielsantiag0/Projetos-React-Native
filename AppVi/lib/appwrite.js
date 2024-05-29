512e5a701921fc0565484c44c0b319c9a03dfd59
gabrielsantiag0 <gabrielleandrosantiago9@gmail.com>
2024-05-29 13:48:33 -0300

    configuring the database (appwrite) and connection file

---
+++ AppVi/lib/appwrite.js
@@ -0,0 +1,9 @@
+export const appwtiteConfig = {
+    endpoint: 'https://cloud.appwrite.io/v1',
+    platform: 'com.jsm.aora',
+    projectId: '665748cb0006bc9851c3',
+    databaseId: '665755500006b3fd5e2f',
+    userCollectionId: '665755d5001d309801a9',
+    videoCollectionId: '665757190032f13ac79d',
+    storageId: '66575afa0029f1125765'
+}
