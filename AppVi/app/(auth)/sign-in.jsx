56122cee248e23088fa2b1c38d65a99ee38005e8
gabrielsantiag0 <gabrielleandrosantiago9@gmail.com>
2024-05-29 11:21:02 -0300

    Finishing the first login screen

--- AppVi/app/(auth)/sign-in.jsx
+++ AppVi/app/(auth)/sign-in.jsx
@@ -1,15 +1,33 @@
-import { Image, SafeAreaView, ScrollView, Text, View } from 'react-native'
-import React from 'react'
+import { Image, ScrollView, Text, View } from 'react-native'
+import React, { useState }  from 'react'
+import { SafeAreaView } from 'react-native-safe-area-context'
+import { Link } from 'expo-router'
 
 import { images } from '../../constants'
 
+import FormField from '../../components/FormField'
+import CustomButton from '../../components/CustomButton'
+
 const SingIn = () => {
+
+  const [Form, setForm] = useState({
+    email: '',
+    password: ''
+  })
+
+  const [isSubmitting, setisSubmitting] = useState(false)
+
+  const submit = () => {
+
+  }
+
   return (
     <SafeAreaView
     className="bg-primary h-full"
     >
      <ScrollView>
       <View
+                                      //min-h-[85vh] \\
       className=" w-full justify-center h-full px-4 my-6"
       >
         <Image 
@@ -22,6 +40,40 @@ const SingIn = () => {
         >
           Log in to Aora
           </Text>
+          <FormField 
+          title="Email"
+          value={Form.email}
+          handleChangeText={(e) => setForm({  ...Form, email: e  })}
+          otherStyles="mt-7"
+          keybordType="email-address"
+          />
+          <FormField 
+          title="Password"
+          value={Form.password}
+          handleChangeText={(e) => setForm({  ...Form, password: e  })}
+          otherStyles="mt-7"
+          />
+          <CustomButton 
+          title="Sign In"
+          handlePress={submit}
+          containerStyles="mt-7"
+          isLoading={isSubmitting}
+          />
+          <View
+          className="justify-center pt-5 flex-row gap-2"
+          >
+            <Text
+            className="text-lg text-gray-100 font-pregular"
+            >
+              Don't have account?
+              </Text>
+              <Link
+              className="text-lg font-psemibold text-secondary"
+              href="/sign-up"
+              >
+                Sign Up
+              </Link>
+          </View>
       </View>
       </ScrollView> 
     </SafeAreaView>
--- AppVi/app/_layout.jsx
+++ AppVi/app/_layout.jsx
@@ -37,6 +37,10 @@ const RootLayout = () => {
  return (
     <Stack>
         <Stack.Screen name='index' options={{ headerShown: false }} />
+        <Stack.Screen name="(auth)"  options={{ headerShown: true }} />
+        <Stack.Screen name='(tabs)' options={{ headerShown: true }} />
+        {/* <Stack.Screen name='/search/[query]' options={{ headerShown: false }} /> */}
+
     </Stack>
  )
 }
